89cea0c8915b427839695805acb78425ac363282
torito602 <torito602@gmail.com>
2025-02-06 18:49:28 -0700

    Add files via upload
    Updated version of AdminHomePage that adds extra functionality to the Invite pane of the UI. This implements the database that's originally created in FirstPage.java

--- TP1/AdminHomePage.java
+++ TP1/AdminHomePage.java
@@ -1,82 +1,219 @@
 package application;
 
+import databasePart1.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
 
-import databasePart1.DatabaseHelper;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+/**
+ * AdminPage class represents the user interface for the admin user.
+ * This page displays a simple welcome message for the admin.
+ */
+
+
 
 public class AdminHomePage {
-
-    // DatabaseHelper instance for fetching users
-    private final DatabaseHelper databaseHelper;
-
-    // TableView to display user info
-    private TableView<User> userTable;
-
-    public AdminHomePage() {
-        this.databaseHelper = new DatabaseHelper();
-    }
-
-    public void show(Stage primaryStage) {
-        VBox layout = new VBox();
-        layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
-        
-        // Label to display the welcome message for the admin
+	/**
+     * Displays the admin page in the provided primary stage.
+     * @param primaryStage The primary stage where the scene will be displayed.
+     */
+	
+	private static final int TIME_EXPIRE = 20; // Code expires in 20 seconds
+	
+    public void show(DatabaseHelper databaseHelper, Stage primaryStage) {
+    	
+    	// Main Layout
+    	BorderPane mainLayout = new BorderPane();	
+	    mainLayout.setStyle("-fx-padding: 20;");
+	    
+	    // Top pane for welcome message
+        VBox topPane = new VBox();
+        topPane.setStyle("-fx-alignment: center; -fx-padding: 10;");
         Label adminLabel = new Label("Hello, Admin!");
         adminLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
-        
-        // Button to view users
-        Button viewUsersButton = new Button("View All Users");
-        viewUsersButton.setOnAction(e -> {
-            // Fetch user data and update the TableView
-            userTable.setItems(fetchUserList());
+        topPane.getChildren().add(adminLabel);
+        mainLayout.setTop(topPane);
+
+        /* Center pane for tabs (each tab represents a sub-pane with functionality)
+        *  This is how our team can organize our individual parts of the project
+        *  rather than trying to add all our parts into one UI
+        */
+        TabPane tabPane = new TabPane();
+
+
+        /*Tabs are objects in which we can add to tabPane to organize different functions for
+        * the user (in this case, admin). A new Tab object gets initialized with a name, and
+        * must add a class to introduce extra functionality to it. User Management should also
+        * not be closable, so as to not have the tab itself disappear.
+        */
+
+        // User Management Tab
+        Tab userManagementTab = new Tab("User Management");
+        userManagementTab.setContent(createUserManagementPane());
+        userManagementTab.setClosable(false);
+
+
+        // Roles Tab
+        Tab rolesTab = new Tab("Edit Roles");
+        rolesTab.setContent(createRolesPane());
+        rolesTab.setClosable(false);
+
+        // Inv-Users Tab
+        Tab inviteUsersTab = new Tab("Invite Users");
+        inviteUsersTab.setContent(createInvitePane(databaseHelper));
+        inviteUsersTab.setClosable(false);
+
+
+        // Temp-Password Tab
+        Tab createPasswordTab = new Tab("Temporary Password");
+        createPasswordTab.setContent(createPasswordPane());
+        createPasswordTab.setClosable(false);
+
+
+        // Add tabs to the tab pane
+        tabPane.getTabs().addAll(userManagementTab, rolesTab, inviteUsersTab, createPasswordTab);
+        mainLayout.setCenter(tabPane);
+
+
+		// Set the scene to primary stage
+        //this creates new window to transition to, and the numbers just means the size of it
+	    Scene adminScene = new Scene(mainLayout, 800, 400);
+	    primaryStage.setScene(adminScene);
+	    primaryStage.setTitle("Admin Page");
+	    primaryStage.show();
+	    
+    }
+
+
+
+    private Pane createUserManagementPane() {
+        VBox userManagementPane = new VBox(10);
+        userManagementPane.setStyle("-fx-padding: 10;");
+
+
+        //this is how buttons are made, and how you can add labels to those buttons
+        Label userLabel = new Label("Users");
+        Button addUserButton = new Button("Add User");
+        Button deleteUserButton = new Button("Delete User");
+        ListView<String> userList = new ListView<>();           //really nice way to list the users in a list of variable '<>' size
+        userList.getItems().addAll("User 1", "User 2", "User 3");   //testing purposes: .getItems() returns a list and adds 3 users.
+
+        // Add functionality to buttons
+        addUserButton.setOnAction(e -> {
+            userList.getItems().add("New User");    //this code should change to invite new user rather than add
         });
 
-        // Setup the TableView
-        userTable = new TableView<>();
-        
-       
-    	//Username Column
-		TableColumn<User, String> userNameCol = new TableColumn<>("Username");
-		userNameCol.setCellValueFactory(cellData -> cellData.getValue().userNameProperty());
-		//Password Column
-		TableColumn<User, String> passCol = new TableColumn<>("Password");
-		passCol.setCellValueFactory(cellData -> cellData.getValue().passwordProperty());
-		//Role Column
-		TableColumn<User, String> roleCol = new TableColumn<>("Role");
-		roleCol.setCellValueFactory(cellData -> cellData.getValue().roleProperty());
-        
-        // Add columns to the TableView
-        userTable.getColumns().addAll(userNameCol, passCol, roleCol);
-        
-        // Add the button and table to the layout
-        layout.getChildren().addAll(adminLabel, viewUsersButton, userTable);
-        
-        // Scene for the admin page
-        Scene adminScene = new Scene(layout, 800, 400);
-        primaryStage.setScene(adminScene);
-        primaryStage.setTitle("Admin Page");
+        /*deleteUserButton is an event handler that defines what happens when
+        * deleteUserButton is clicked. It uses that 'e -> {...}' expression to
+        * handle that event. Same concept applies to addUserButton
+        */
+        deleteUserButton.setOnAction(e -> {
+            String selectedUser = userList.getSelectionModel().getSelectedItem();   //deletes the user that is currently selected
+            if (selectedUser != null) {             //if a user is selected, it will go through the getItems
+                userList.getItems().remove(selectedUser);   //.getItems returns a list, in this case the userlist, and selects the selectedUser
+            }
+        });
+
+        /*userManagementPane finally implements the getChildren function, which adds multiple UI
+        * elements (userLabel, addUserButton, deleteUserButton, userList) into the Vbox
+        * getChildren() essentially accesses these elements, and AddAll() put them together in a 
+        * vertical stack (that's why its called Vbox, it organizes stuff vertically)
+        */ 
+        userManagementPane.getChildren().addAll(userLabel, addUserButton, deleteUserButton, userList);
+        return userManagementPane;
     }
 
-    // Fetch user data from the database and return it as an ObservableList
-    private ObservableList<User> fetchUserList() {
-        ObservableList<User> users = FXCollections.observableArrayList();
-        try {
-            ResultSet rs = databaseHelper.getAllUsers();
-            while (rs.next()) {
-                // Populate the ObservableList with User objects
-                users.add(new User(rs.getString("userName"), rs.getString("password"), rs.getString("role")));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return users;
+
+
+    private Pane createRolesPane() {
+        VBox rolesPane = new VBox(10);
+        rolesPane.setStyle("-fx-padding: 10;");
+
+
+        //ADD FUNCTION HERE
+
+        return rolesPane;
     }
+
+
+
+    private Pane createInvitePane(DatabaseHelper databaseHelper) {
+        
+        VBox invitePane = new VBox(10);
+        invitePane.setStyle("-fx-padding: 10;");
+
+        
+        // Label to display the title of the page
+        Label userLabel = new Label("Invite ");
+        //userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+        
+        // Button to generate the invitation code
+        Button showCodeButton = new Button("Generate Invitation Code");
+        
+        // Label to display the generated invitation code
+        Label inviteCodeLabel = new Label(""); ;
+        inviteCodeLabel.setStyle("-fx-font-size: 14px; -fx-font-style: italic;");
+        
+        // Label to display the timer
+        Label myTimer = new Label("");
+        myTimer.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
+        
+        showCodeButton.setOnAction(a -> {
+            // Generate the invitation code using the databaseHelper and set it to the label
+            String invitationCode = databaseHelper.generateInvitationCode();
+            inviteCodeLabel.setText(invitationCode);
+
+            // Start the countdown timer
+            startCountdown(myTimer);
+        });
+        
+
+        invitePane.getChildren().addAll(userLabel, showCodeButton, inviteCodeLabel, myTimer);
+
+
+        return invitePane;
+    }
+
+    private void startCountdown(Label myTimer) {
+        // Set the initial time remaining
+        int[] timeRemaining = {TIME_EXPIRE};
+
+        // Create a Timeline to update the timer every second
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            timeRemaining[0]--;
+
+            if (timeRemaining[0] >= 0) {
+                // Update the timer label
+                myTimer.setText("Code expires in: " + timeRemaining[0] + " seconds");
+            } 
+            else {
+                // Stop the timer and indicate the code has expired
+                myTimer.setText("Code expired!");
+                ((Timeline) event.getSource()).stop();
+            }
+        }));
+
+        // Set the timeline to run indefinitely until stopped
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+
+
+    private Pane createPasswordPane() {
+        VBox passwordPane = new VBox(10);
+        passwordPane.setStyle("-fx-padding: 10;");
+
+
+        //ADD FUNCTION HERE
+
+        return passwordPane;
+    }
+
 }
